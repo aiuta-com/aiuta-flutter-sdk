5387803a81ab93480c737a5ed6b957e5ee396134
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-04-30 22:29:41 +0200

    Add camera to sample

--- example/lib/defaults/features/picker/aiuta_image_picker_feature_default.dart
+++ example/lib/defaults/features/picker/aiuta_image_picker_feature_default.dart
@@ -1,11 +1,12 @@
 import 'package:aiuta_flutter/configuration/features/image_picker/aiuta_image_picker_feature.dart';
 import 'package:aiuta_flutter/configuration/features/image_picker/aiuta_image_picker_images.dart';
 import 'package:aiuta_flutter/configuration/features/image_picker/aiuta_image_picker_strings.dart';
+import 'package:aiutasdk_example/defaults/features/picker/camera/aiuta_image_picker_camera_feature_default.dart';
 import 'package:aiutasdk_example/defaults/features/picker/gallery/aiuta_image_picker_photo_gallery_feature_default.dart';
 
 final class ImagePickerFeatureBuilder {
   AiutaImagePickerFeature build() => AiutaImagePickerFeature(
-        camera: null,
+        camera: ImagePickerCameraFeatureBuilder().build(),
         photoGallery: ImagePickerPhotoGalleryFeatureBuilder().build(),
         predefinedModels: null,
         uploadsHistory: null,
